e2b18914b57a92ba30b79c0f3813f53085191a49
Michael Johnson <migarjo@github.com>
2017-07-19 17:17:02 -0600

    Add slack adapter option to bin/hubot

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "universe-bot" %* 
+node_modules\.bin\hubot.cmd --name "universe-bot" %* -a slack
